3ce69f9edec36a97298137ce5e42b8688588a1ad
et990 <et990dave@hotmail.com>
2019-08-02 14:38:57 +0200

    style: Home

--- src/app/shared/custom-material.module.ts
+++ src/app/shared/custom-material.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatGridListModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
   ]
 })
 export class CustomMaterialModule {
   static exportModules = [
     MatGridListModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
   ];
 }
