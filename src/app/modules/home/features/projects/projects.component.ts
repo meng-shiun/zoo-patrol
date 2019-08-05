d788779632aba037bf7e462d3788d33495a11bf1
et990 <et990dave@hotmail.com>
2019-08-05 12:35:07 +0200

    refactor: Use mat-tab-link for routerlink

--- src/app/modules/home/features/projects/projects.component.ts
+++ src/app/modules/home/features/projects/projects.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ITab {
+  name: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-  tabs: string[] = ['My projects', 'Archived projects', '+ New project'];
+  tabs: ITab[] = [
+    { name: 'Running projects', link: 'running' },
+    { name: 'My projects', link: 'my_projects' },
+    { name: 'Archived projects', link: 'archived' },
+    { name: '+ New projects', link: 'new' }
+  ];
+
+  activeTab = this.tabs[0].name;
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
