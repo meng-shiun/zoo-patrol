e559b3cc0b4f7a5206b2a18d418e0c229d417765
et990 <et990dave@hotmail.com>
2019-08-07 15:23:15 +0200

    feat: Add project interfaces

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -2,3 +2,31 @@ export interface ITab {
   name: string;
   link: string;
 }
+
+export interface IProject {
+  id: number;
+  client: string;
+  name: string;
+  manager: string;
+  status: string;
+  deadline: string;
+}
+
+export interface IProjectDetails {
+  id: number;
+  client: string;
+  name: string;
+  manager: string;
+  status: string;
+}
+
+export interface IProjectPlanning {
+  id: number;
+  description: string;
+}
+
+export interface IProjectBudget {
+  id: number;
+  budget: number;
+  type: string;
+}
