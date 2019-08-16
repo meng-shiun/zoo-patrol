af624ac37c0dbdeb700bccfd13f590edcdaa8620
et990 <et990dave@hotmail.com>
2019-08-16 14:25:41 +0200

    tweak: Add ActionReducerMap type to reducers

--- src/app/store/app.store.ts
+++ src/app/store/app.store.ts
@@ -1,9 +1,11 @@
+import { ActionReducerMap } from '@ngrx/store';
+
 import * as fromDashboard from '@app/modules/home/dashboard/store/dashboard.reducer';
 
 export interface AppState {
   dashboard: fromDashboard.State;
 }
 
-export const reducers = {
+export const reducers: ActionReducerMap<AppState> = {
   dashboard: fromDashboard.reducer
 };
