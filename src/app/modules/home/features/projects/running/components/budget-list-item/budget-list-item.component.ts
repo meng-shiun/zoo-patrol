bdca4c1967a3d8af78fc7ae33f046fe7c6a5a552
et990 <et990dave@hotmail.com>
2019-09-02 16:57:29 +0200

    chore: Import FormModule

--- src/app/modules/home/features/projects/running/components/budget-list-item/budget-list-item.component.ts
+++ src/app/modules/home/features/projects/running/components/budget-list-item/budget-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { IBudgetItem } from '@app/shared';
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CustomMaterialModule } from './custom-material.module';
 
@@ -7,10 +8,14 @@ import { CustomMaterialModule } from './custom-material.module';
 @NgModule({
   declarations: [],
   imports: [
-    CustomMaterialModule
+    CustomMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     CustomMaterialModule.exportModules
   ]
 })
