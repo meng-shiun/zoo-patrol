d620f235cf6f1681cdf9a303ac5107126e05062b
et990 <et990dave@hotmail.com>
2019-09-27 11:25:36 +0200

    feat: Apply Angular material on project filters

--- src/app/modules/home/features/projects/running/components/project-filter-bar/project-filter-bar.component.ts
+++ src/app/modules/home/features/projects/running/components/project-filter-bar/project-filter-bar.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-filter-bar.component.scss']
 })
 export class ProjectFilterBarComponent implements OnInit {
+  clients: any[] = ['Watsica LLC', 'Veum Inc', 'Frami-Ledner'];
+  managers: any[] = ['Jordy', 'Kristof', 'Chelsey', 'Tom'];
+  status: any[] = [140, 200, 605];
+  sortBy: any[] = ['new', 'old'];
 
   constructor() { }
 
